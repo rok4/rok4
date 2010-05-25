972a7f34d720b726e2dbcaa76ec2e859a69b2f9c
nviel <devnull@localhost>
2010-05-25 15:06:20 +0000

    Evolution du Logger: Petit refactor. Le niveau de log minimum est maintenant pris en compte.

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -7,69 +7,135 @@
 #include <cstdio>
 #include <iostream>
 
-const char* LogLevelText[5] = {"DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
 
-class logbuffer : public std::stringbuf {
-  private:
-  static std::ostream *out;
-  static pthread_mutex_t mutex;
-  pthread_t thread_id;
-  const char* level;
+/** L'objet de cette classe est de permettre des écritures concurentes sur le
+ * flux de log. De sorte qu'il n'est pas possible que plusieurs threads écrivent
+ * sur même ligne.
+ * Elle s'occupe de plus de l'horodatage des lignes de log.
+ */
+/*----------------------------------------------------------------------------*/
+class LogBuffer: public std::stringbuf {
+private:
+	static std::ostream *out;
+	static pthread_mutex_t mutex;
 
-  protected:
-  virtual int sync();
-  public:
-  logbuffer(LogLevel level) : std::stringbuf(std::ios_base::out), thread_id(pthread_self()), level(LogLevelText[level]) {}
+protected:
+	virtual int sync();
+
+public:
+	LogBuffer(LogLevel level) :	std::stringbuf(std::ios_base::out) {};
+	void setOut(std::ostream *out);
 };
 
-std::ostream *logbuffer::out = &std::cerr; // Définition du flux de sortie des log
-pthread_mutex_t logbuffer::mutex = PTHREAD_MUTEX_INITIALIZER;
+/* TODO NV: cette valeur est à conservée jusqu'a ce qu'on ai pu lire dans le fichier
+ *           de config le fichier de log voulu par l'utilisateur et qu'on a eu
+ *           confirmation qu'on peut écrire dedans.
+ */
+std::ostream *LogBuffer::out = &std::cerr; // Définition du flux de sortie des log
 
+pthread_mutex_t LogBuffer::mutex = PTHREAD_MUTEX_INITIALIZER;
+/** Cette fonction est appelée automatiquement lors du flush.
+ *  Elle définit la manière dont le message est écrit dans le fichier
+ *  de log (membre "out").
+ *  Etant en contexte multithread, elle interdit l'écriture simultanée
+ *  par des threads différents.
+ */
+int LogBuffer::sync() {
+	pthread_mutex_lock(&mutex);
+	out->write(pbase(), pptr() - pbase());
+	pthread_mutex_unlock(&mutex);
 
-
-
-int logbuffer::sync() {    
-  timeval tim;
-  gettimeofday(&tim, NULL);
-  char date[32+1];
-  tm *now = localtime(&tim.tv_sec);
-  //FIXME:NV:cause de plantage 64bit et pb de sécurité
-  sprintf(date, "%s %04d/%02d/%02d %02d:%02d:%02d.%06d", level, now->tm_year+1900, now->tm_mon+1, now->tm_mday, now->tm_hour, now->tm_min, now->tm_sec, (int) (tim.tv_usec));
-
-  pthread_mutex_lock(&mutex);
-    (*out) << date << "\t" << thread_id << "\t";
-    out->write(pbase(), pptr() - pbase());
-  pthread_mutex_unlock(&mutex);
-
-  str("");
-  return 0;
+	str(""); //vide le stringBuffer (puisqu'on l'a copié)
+	return 0;
 }
 
 
 
+/** On dispose grace à cette classe d'un "stream" qui ignore tout ce qu'on lui
+ * envoie.
+ */
+class NullStream : public std::ostream{
+};
+
+template<class T>
+NullStream& operator<<(NullStream &stream, T &message) {
+	return stream;
+};
+
+/*------------------------ gestion des threads -------------------------------*/
 
 static pthread_once_t key_once = PTHREAD_ONCE_INIT;
-static pthread_key_t logger_key[5];
+
+static pthread_key_t logger_key[NB_ACTION];
+
+/* on cree une clef par action possible (écrire ou ignorer le message)*/
 static void init_key() {
-  for(int i = 0; i < 5; i++) 
-    pthread_key_create(&logger_key[i], 0);
-}
-
-std::ostream &Logger(LogLevel level) {
-  pthread_once(&key_once, init_key); // initialize une seule fois logger_key
-
-  std::ostream *L;
-  if ((L = (std::ostream*) pthread_getspecific(logger_key[level])) == 0) {
-    L = new std::ostream(new logbuffer(level));
-    pthread_setspecific(logger_key[level], (void*) L);
-  }
-  return *L;
+	for (int i = 0; i < NB_ACTION; i++)
+		pthread_key_create(&logger_key[i], NULL);
 }
 
 
-class NullStream {};
-template<class T> 
-NullStream& operator<<(NullStream &stream, T &message) {return stream;};
+/* -----------implémentation de la classe principale: Logger -----------------*/
 
+LogLevel Logger::minLevel = WARN;
+
+const int NBLEVEL = 5;
+const char* logLevelName[NBLEVEL] = { "DEBUG", "INFO ", "WARN ", "ERROR", "FATAL" };
+
+LogLevel Logger::getMinLevel() {
+	return minLevel;
+}
+
+void Logger::setMinLevel(LogLevel const minLevel) {
+	Logger::minLevel = minLevel;
+}
+
+string Logger::getLogFileName(){
+	return logFileName;
+}
+
+void Logger::setLogFileName(string fileName){
+	logFileName=fileName;
+	/*TODO: tester l'accès au fichier*/
+}
+
+/** Renvoie une référence sur le std::ostream qui correspond -pour le processus
+ * en cours- au niveau de log "level". Il est alors possible d'écrire dedans
+ * simplement ainsi:
+ *  Logger(WARN) << "message du warning" << std::endl;     */
+std::ostream &Logger::logStream(LogLevel level) {
+	pthread_once(&key_once, init_key); // initialize une seule fois logger_key
+
+	std::ostream *L;
+	Action action;
+	timeVal time;
+	tm *now;
+
+	if (level>=minLevel) action=WRITE; else action=IGNORE;
+
+	if ((L = (std::ostream*) pthread_getspecific(logger_key[action])) == NULL) {
+		if (action==IGNORE){
+			L = new NullStream;
+		}else{
+			L = new std::ostream(new LogBuffer(level));
+		}
+		pthread_setspecific(logger_key[action], (void*) L);
+	}
+
+	if (action==WRITE){
+		/* mise en forme des informations affichées par défaut */
+		gettimeofday(&time, NULL);
+		now = localtime(&time.tv_sec);
+		char date[26 + 1];
+		sprintf(date, "%04d/%02d/%02d %02d:%02d:%02d.%06d",
+			now->tm_year + 1900, now->tm_mon + 1, now->tm_mday, now->tm_hour,
+			now->tm_min, now->tm_sec, (int) (tim.tv_usec));
+
+		*L << logLevelName[level] <<" "<< date << " " << pthread_self() << " ";
+	}
+
+	return *L;
+
+}
 
 
--- src/Logger.h
+++ src/Logger.h
@@ -5,8 +5,26 @@
 
 
 typedef enum{DEBUG = 0, INFO = 1, WARN = 2, ERROR = 3, FATAL = 4} LogLevel;
-std::ostream &Logger(LogLevel l);
+typedef enum{IGNORE = 0, WRITE = 1} Action;
+#define NB_ACTION 2
 
-#define LOGGER(x) Logger(x)<<" "<<__FILE__<<":"<<__LINE__<<" in "<<__FUNCTION__<<" "
+class Logger{
+private:
+	static int const NBLEVEL;
+	static char* const logLevelName[];
+	static LogLevel minLevel; // niveau de log minimum écrit dans le fichier de log.
+	static string logFileName;
+
+public:
+	static LogLevel getMinLevel();
+	static void setMinLevel(LogLevel const minLevel);
+	static string getLogFileName();
+	static void setLogFileName(string fileName);
+
+	static std::ostream &logStream(LogLevel level);
+};
+
+
+#define LOGGER(x) (Logger::getMinLevel()==DEBUG ? Logger::logStream(x) << "["<<__FILE__<<":"<<__LINE__<<" in "<<__FUNCTION__<<"]:" : Logger::logStream(x) )
 
 #endif
