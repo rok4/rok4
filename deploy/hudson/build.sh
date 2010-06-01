d774ee7cbe01bbb7af88601042076d6182db19db
gcebelieu <devnull@localhost>
2010-06-01 09:34:34 +0000

    integration des scripts hudson + configurations de deploiement dans le svn

---
+++ deploy/hudson/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# LD_LIBRARY_PATH pour librairies proj4
+export LD_LIBRARY_PATH=/usr/local/lib
+
+# building and testing
+cd src
+echo "\n\n\n\n***  BUILD AND TESTS    *****"
+make cpputest
+
+# documentation
+echo "\n\n\n\n***  DOCUMENTATION    *****"
+make doc
+
+# deploiement
+ROK4BASE=/var/www/hudson/rok4
+echo "\n\n\n\n***  DEPLOIEMENT    *****"
+cd ../target
+rm -f $ROK4BASE/bin/WMSServer
+cp bin/WMSServer $ROK4BASE/bin/
+cp -r docs/* $ROK4BASE/docs/
+rm -f $ROK4BASE/builds/rok4-rev${SVN_REVISION}.tgz
+tar -cvzf rok4-rev${SVN_REVISION}.tgz bin/WMSServer docs
+mv rok4-rev${SVN_REVISION}.tgz $ROK4BASE/builds/
+
