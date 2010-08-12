ac432433f084c0bcee26f90aa9a4d4903fd91795
nviel <devnull@localhost>
2010-08-12 15:11:09 +0000

    Corrections importantes dans le getLine() de ExtendedCompoundImage.

--- src/ExtendedCompoundImage.h
+++ src/ExtendedCompoundImage.h
@@ -1,5 +1,5 @@
-#ifndef COMPOUND_IMAGE_H
-#define COMPOUND_IMAGE_H
+#ifndef EXTENTED_COMPOUND_IMAGE_H
+#define EXTENTED_COMPOUND_IMAGE_H
 
 #include "GeoreferencedImage.h"
 #include <vector>
@@ -16,71 +16,89 @@
 
 class ExtendedCompoundImage : public GeoreferencedImage {
 
-  friend class extendedCompoundImageFactory;
+	friend class extendedCompoundImageFactory;
 
-  private:
+private:
 
-  std::vector<GeoreferencedImage*> images;
+	std::vector<GeoreferencedImage*> images;
 
-  /**
-  Remplissage iteratif d une ligne
-  Copie de la portion recouvrante de chaque ligne d une image dans l image finale
-  */
+	/**
+	Remplissage iteratif d'une ligne
+	Copie de la portion recouvrante de chaque ligne d'une image dans l'image finale
+	retourne le nombre d'octets de la ligne
+	*/
 
-  template<typename T>
-  int _getline(T* buffer, int line) {
+	template<typename T>
+	int _getline(T* buffer, int line) {
 
-   double y=l2y(line);
-   for (int i=0;i<images.size()-1;i++)
-   {
-        if (images[i]->getymin()>y||images[i]->getymax()<y)
-                continue;
-        else if (images[i]->getxmin()>getxmax()||images[i]->getxmax()<getxmin())
-                continue;
+		double y=l2y(line);
+		for (uint i=0;i<images.size();i++){
+			/* On écarte les images qui ne se trouvent pas sur la ligne*/
+			if (images[i]->getymin()>=y||images[i]->getymax()<=y)
+				continue;
+			if (images[i]->getxmin()>=getxmax()||images[i]->getxmax()<=getxmin())
+				continue;
 
-        int c0=__max(0,x2c(images[i]->getxmin()));
-        int c1=__min(width,x2c(images[i]->getxmax()));
-        T* buffer_t = new T[images[i]->width*images[i]->channels];
-        images[i]->getline(buffer_t,images[i]->y2l(y));
-        memcpy(&buffer[c0],&buffer_t[__min(0,-x2c(images[i]->getxmin()))],(c1-c0)*channels);
-        delete [] buffer_t;
-   }
-    return width*channels;
-  }
+			/* c0 est la première colonne de l'ExtendedCompoundImage remplie par l'image en cours.*/
+			int c0=__max(0,x2c(images[i]->getxmin()));
+			/* c1-1 est la dernière colonne de l' ExtendedCompoundImage remplie par l'image en cours.*/
+			/* FIXME: NV: j'ai quand même un doute sur le fait que ce ne soit pas parfois c1*/
+			int c1=__min(width,x2c(images[i]->getxmax()));
+			T* buffer_t = new T[images[i]->width*images[i]->channels];
+			images[i]->getline(buffer_t,images[i]->y2l(y));
 
-  protected:
+			memcpy(&buffer[c0*channels],
+				   &buffer_t[-(__min(0,x2c(images[i]->getxmin())))*channels],
+				   (c1-c0)*channels*sizeof(T));
 
-  /** Constructeur
+//			if (line==0 || line==1 || line==2000 || line==2010){
+//				cout<<"image:"<<i<<endl;
+//				cout<<"line"<<line<<endl;
+//				cout<<"c0:"<<c0<<" c1:"<<c1<<endl;
+//				cout<<"@ dans buffer (px):"<< c0<<endl;
+//				cout<<"@ dans line   (px):"<< -__min(0,x2c(images[i]->getxmin())) <<endl;
+//				cout<<"taille de la copie (px):"<< c1-c0 <<endl;
+//				cout<<"---------------------------" <<endl;
+//			}
+
+			delete [] buffer_t;
+		}
+		return width*channels*sizeof(T);
+	}
+
+protected:
+
+	/** Constructeur
   Appelé via une fabrique de type extendedCompoundImageFactory
   Les GeoreferencedImage sont detruites ensuite en meme temps que l'objet
   Il faut donc les creer au moyen de l operateur new et ne pas s'occuper de leur suppression
-  */
-  ExtendedCompoundImage(int width, int height, int channels, double x0, double y0, double resx, double resy, std::vector<GeoreferencedImage*>& images) :
-        GeoreferencedImage(width, height, channels,x0,y0,resx,resy),
-        images(images) {}
+	 */
+	ExtendedCompoundImage(int width, int height, int channels, double x0, double y0, double resx, double resy, std::vector<GeoreferencedImage*>& images) :
+		GeoreferencedImage(width, height, channels,x0,y0,resx,resy),
+		images(images) {}
 
-  public:
+public:
 
-  /** D */
-  int getline(uint8_t* buffer, int line) { return _getline(buffer, line); }
+	/** D */
+	int getline(uint8_t* buffer, int line) { return _getline(buffer, line); }
 
-  /** D */
-  int getline(float* buffer, int line) { return _getline(buffer, line); }
+	/** D */
+	int getline(float* buffer, int line) { return _getline(buffer, line); }
 
-  /** Destructeur
+	/** Destructeur
       Suppression des images */
-  virtual ~ExtendedCompoundImage() {
-      for(int i = 0; i < images.size(); i++)
-        delete images[i];
-  }
+	virtual ~ExtendedCompoundImage() {
+		for(uint i = 0; i < images.size(); i++)
+			delete images[i];
+	}
 
 };
 
 class extendedCompoundImageFactory {
-  public:
-        ExtendedCompoundImage* createExtendedCompoundImage(int width, int height, int channels, double x0, double y0, double resx, double resy, std::vector<GeoreferencedImage*>& images)
+public:
+	ExtendedCompoundImage* createExtendedCompoundImage(int width, int height, int channels, double x0, double y0, double resx, double resy, std::vector<GeoreferencedImage*>& images)
 	{
-		int i;
+		uint i;
 		double intpart;
 		for (i=0;i<images.size()-1;i++)
 		{
@@ -90,10 +108,10 @@ class extendedCompoundImageFactory {
 				return NULL;				
 			}
 			if (modf(images[i]->getxmin()/images[i]->getresx(),&intpart)!=modf(images[i+1]->getxmin()/images[i+1]->getresx(),&intpart)
-			|| modf(images[i]->getymax()/images[i]->getresy(),&intpart)!=modf(images[i+1]->getymax()/images[i+1]->getresy(),&intpart))
+					|| modf(images[i]->getymax()/images[i]->getresy(),&intpart)!=modf(images[i+1]->getymax()/images[i+1]->getresy(),&intpart))
 			{
 				LOGGER_DEBUG("Les images ne sont pas toutes en phase");
-                                return NULL;				
+				return NULL;
 			}	
 		}
 
