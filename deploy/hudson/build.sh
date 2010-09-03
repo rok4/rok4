f142931af958b2e7a8b33583e5cd9240ce9af484
gcebelieu <devnull@localhost>
2010-09-03 06:48:50 +0000

    make very-clean test au lieu d'un clean

--- deploy/hudson/build.sh
+++ deploy/hudson/build.sh
@@ -6,7 +6,7 @@
 # building and testing
 cd rok4
 echo "\n\n\n\n***  BUILD AND TESTS    *****"
-make clean test
+make very-clean test
 if [ $? -ne 0 ] ; then
   exit 1
 fi
@@ -23,13 +23,15 @@ fi
 # deploiement
 ROK4BASE=/var/www/hudson/rok4
 echo "\n\n\n\n***  DEPLOIEMENT    *****"
-cd ../target
+
 rm -fr $ROK4BASE/bin/*
-cp bin/rok4 $ROK4BASE/bin/
 rm -fr $ROK4BASE/config/*
+rm -f $ROK4BASE/builds/rok4-rev${SVN_REVISION}.tgz
+
+cd ../target
+cp bin/rok4 $ROK4BASE/bin/
 cp -r config/* $ROK4BASE/config/
 cp -r docs/* $ROK4BASE/docs/
-rm -f $ROK4BASE/builds/rok4-rev${SVN_REVISION}.tgz
 tar -cvzf rok4-rev${SVN_REVISION}.tgz bin/rok4 docs
 mv rok4-rev${SVN_REVISION}.tgz $ROK4BASE/builds/
 
