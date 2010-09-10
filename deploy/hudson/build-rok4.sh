4534e07276138cda9ee7e99347cacd7031a88991
gcebelieu <devnull@localhost>
2010-09-10 06:41:02 +0000

    arret et redemarrage du service redemarrage du service apache à chaque build

--- deploy/hudson/build-rok4.sh
+++ deploy/hudson/build-rok4.sh
@@ -24,6 +24,12 @@ fi
 ROK4BASE=/var/www/hudson/rok4
 echo "\n\n\n\n***  DEPLOIEMENT    *****"
 
+echo "arret serveur apache..."
+sudo /etc/init.d/apache2 stop
+if [ -f /var/run/apache2.pid ] ; then
+  echo "[warning] Pb lors de l'arret du service apache ! "
+fi
+
 rm -fr $ROK4BASE/bin/*
 rm -fr $ROK4BASE/config/*
 rm -f $ROK4BASE/builds/rok4-rev${SVN_REVISION}.tgz
@@ -34,4 +40,11 @@ cp -r config/* $ROK4BASE/config/
 cp -r docs/* $ROK4BASE/docs/
 tar -cvzf rok4-rev${SVN_REVISION}.tgz bin/rok4 docs
 mv rok4-rev${SVN_REVISION}.tgz $ROK4BASE/builds/
+
+echo "demarrage serveur apache..."
+sudo /etc/init.d/apache2 start
+if [ ! -f /var/run/apache2.pid ] ; then
+  echo "[error] Pb lors du démarrage du service apache ! "
+  exit 3
+fi
 
