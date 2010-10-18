9d044cd22a4a756120df9313aaffc2809fa53591
schataigner <devnull@localhost>
2010-10-18 09:40:02 +0000

    Correction shell

--- deploy/hudson/build-be4.sh
+++ deploy/hudson/build-be4.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # building and testing
-cd be4
+cd libs
 echo "\n\n\n\n***  BUILD AND TESTS    *****"
 make
 if [ $? -ne 0 ] ; then
   exit 1
+fi
+
+cd ../be4
+echo "\n\n\n\n***  BUILD AND TESTS    *****"
+make
+if [ $? -ne 0 ] ; then
+  exit 2
 fi
