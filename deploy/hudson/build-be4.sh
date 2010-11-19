bc5c8b19026228ce4294b1d74657a3041c9f482b
schataigner <devnull@localhost>
2010-11-19 14:57:01 +0000

    Modif build-be4.sh

--- deploy/hudson/build-be4.sh
+++ deploy/hudson/build-be4.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+dir=`pwd`
+
 # building and testing
 cd lib
 echo "\n\n\n\n***  BUILD LIBS   *****"
@@ -8,6 +10,7 @@ if [ $? -ne 0 ] ; then
   exit 1
 fi
 
+cd $dir
 cd be4
 echo "\n\n\n\n***  BUILD BE4   *****"
 make
