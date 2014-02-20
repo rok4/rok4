226314b83c01e58500992f52bf8bbc02bfb9c375
tsatabin <devnull@localhost>
2014-02-20 14:28:23 +0100

    compilation avec kakadu ok

--- cmake/Modules/FindKAKADU.cmake
+++ cmake/Modules/FindKAKADU.cmake
@@ -3,32 +3,33 @@
 #
 # If it's found it sets KAKADU_FOUND to TRUE
 # and following variables are set:
-#    KAKADU_INCLUDE_DIR
-#    KAKADU_LIBRARY
+#    JPEG2000_INCLUDE_DIR
+#    KAKADU_LIBRARY_1
+#    KAKADU_LIBRARY_2
 
-FIND_PATH(KAKADU_INCLUDE_DIR jp2.h
+FIND_PATH(JPEG2000_INCLUDE_DIR jp2.h
     /usr/local/include 
     /usr/include 
     c:/msys/local/include
     C:/dev/cpp/libopenjpeg/src
     /home/theo/TEST/v6_4_1-00841C/managed/all_includes
     )
-FIND_LIBRARY(KAKADU_LIBRARY NAMES libkdu_v64R.so PATHS 
+
+FIND_LIBRARY(KAKADU_LIBRARY_1 NAME libkdu_aux.a PATHS 
     /usr/local/lib 
     /usr/lib 
     c:/msys/local/lib
     C:/dev/cpp/libkakadu/src
     /home/theo/TEST/v6_4_1-00841C/lib/Linux-x86-64-gcc
     )
-#FIND_LIBRARY(KAKADU_LIBRARY kakadu)
 
-MESSAGE("KAKADU_INCLUDE_DIR : ${KAKADU_INCLUDE_DIR}")
-MESSAGE("KAKADU_LIBRARY : ${KAKADU_LIBRARY}")
-  
-IF(CPPUNIT_LIBRARY)
-    SET(CPPUNIT_LIBRARY ${KAKADU_LIBRARY} ${CMAKE_DL_LIBS})
-ENDIF(CPPUNIT_LIBRARY)
-
+FIND_LIBRARY(KAKADU_LIBRARY_2 NAME libkdu.a PATHS 
+    /usr/local/lib 
+    /usr/lib 
+    c:/msys/local/lib
+    C:/dev/cpp/libkakadu/src
+    /home/theo/TEST/v6_4_1-00841C/lib/Linux-x86-64-gcc
+    )
 
 INCLUDE( "FindPackageHandleStandardArgs" )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( "Kakadu" DEFAULT_MSG KAKADU_INCLUDE_DIR KAKADU_LIBRARY )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( "Kakadu" DEFAULT_MSG JPEG2000_INCLUDE_DIR KAKADU_LIBRARY_1 KAKADU_LIBRARY_2 )
