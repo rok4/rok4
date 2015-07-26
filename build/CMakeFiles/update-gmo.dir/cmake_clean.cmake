1a4da5166a3b966e41cea9851fa133555202835d
Thibbo <thibault.coupin@gmail.com>
2015-07-26 12:01:09 +0200

    Remove build dir

--- build/CMakeFiles/update-gmo.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/update-gmo"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/update-gmo.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/lib/libpng/CMakeFiles/png.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-FILE(REMOVE_RECURSE
-  "libpng16.a"
-)
