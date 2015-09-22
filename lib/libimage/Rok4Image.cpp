0eb6038e63c139d9eed77fc47408203c1640ec56
tsatabin <devnull@localhost>
2015-09-22 18:23:58 +0200

    Correction d'un mauvais merge :
    delete[] tile; dédoublé dans Rok4Image.cpp
    
    Ce soucis avait déjà été rencontré sur la branche develop : corrigé par lgrondin (révision 4d201cfa1fe2)

--- lib/libimage/Rok4Image.cpp
+++ lib/libimage/Rok4Image.cpp
@@ -645,7 +645,7 @@ int Rok4Image::writeImage ( Image* pIn, bool crop )
 
         for ( int y = 0; y < tileHeightwise; y++ ) {
             // On récupère toutes les lignes pour cette ligne de tuiles
-          for (int lig = 0; lig < tileHeight; lig++) {
+            for (int lig = 0; lig < tileHeight; lig++) {
                 if (pIn->getline(lines + lig*imageLineSize, y*tileHeight + lig) == 0) {
                     LOGGER_ERROR("Error reading the source image's line " << y*tileHeight + lig);
                     return -1;                    
@@ -722,8 +722,6 @@ int Rok4Image::writeImage ( Image* pIn, bool crop )
     }
     
     delete [] tile;
-    
-    delete[] tile;
 
     if (! close()) {
         LOGGER_ERROR("Cannot close the ROK4 images (write index and clean) for " << filename);
