ac4ee02b7d9236e52b2276d8cb0ffc8176b86a80
Matthieu PROBOEUF <matthieu.proboeuf@atos.net>
2015-09-24 14:37:46 +0200

    Update version to 0.25.2.PATCH

--- lib/libtiff/tiff-3.8.2/libtiff/tif_unix.c
+++ lib/libtiff/tiff-3.8.2/libtiff/tif_unix.c
@@ -171,7 +171,7 @@ TIFFOpen(const char* name, const char* mode)
 	fd = open(name, m, 0666);
 #endif
 	if (fd < 0) {
-		TIFFErrorExt(0, module, "%s: Cannot open. FD", name);
+		TIFFErrorExt(0, module, "%s: Cannot open. FD : %s", name, fd);
 		return ((TIFF *)0);
 	}
 
