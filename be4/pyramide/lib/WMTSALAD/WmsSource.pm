a875c7139783366e2c0c1dc1852f586c1904fa6f
xdumetz <devnull@localhost>
2016-02-18 11:25:17 +0100

    _load() is now implemented.

--- be4/pyramide/lib/WMTSALAD/WmsSource.pm
+++ be4/pyramide/lib/WMTSALAD/WmsSource.pm
@@ -80,7 +80,15 @@ use constant FALSE => 0;
 ################################################################################
 
 BEGIN {}
-INIT {}
+INIT {
+    %WMS = (
+        format => ['image/png','image/tiff','image/x-bil;bits=32','image/tiff&format_options=compression:deflate','image/tiff&format_options=compression:lzw','image/tiff&format_options=compression:packbits','image/tiff&format_options=compression:raw'],
+        version => ['1.1.1', '1.3.0'],
+    );
+
+    %DEFAULT = (
+        min_size => 0,
+    );}
 END {}
 
 ####################################################################################################
@@ -147,24 +155,24 @@ sub new() {
     # IMPORTANT : if modification, think to update natural documentation (just above)
     # see config/pyramids/pyramid.xsd to get the list of parameters, as used by Rok4.
     my $self = {
-        wms_url => undef,
-        wms_proxy => undef,
-        wms_timeout => undef,
-        wms_retry => undef,
-        wms_interval => undef,
-        wms_user => undef,
-        wms_password => undef,
-        wms_referer => undef,
-        wms_userAgent => undef,
-        wms_version => undef,
-        wms_layers => undef,
-        wms_styles => undef,
-        wms_crs => undef,
-        wms_format => undef,
-        wms_channels => undef,
-        wms_nodata => undef,
-        wms_extent => undef,
-        wms_option => undef,
+        url => undef,
+        proxy => undef,
+        timeout => undef,
+        retry => undef,
+        interval => undef,
+        user => undef,
+        password => undef,
+        referer => undef,
+        userAgent => undef,
+        version => undef,
+        layers => undef,
+        styles => undef,
+        crs => undef,
+        format => undef,
+        channels => undef,
+        nodata => undef,
+        extent => undef,
+        option => undef,
     };
 
     bless($self, $class);
@@ -236,22 +244,40 @@ sub _load() {
     my $self = shift;
     my $params = shift;
 
-    # Test if the mandatory parameters are defined
+
+    foreach my $key (keys $params) {
+        chomp $params->{$key};
+    }
+
+    #### Mandatory parameters ####
+    
     ## server URL
-    if (!exists $params->{wms_url} || !defined $params->{wms_url} || $params->{wms_url} eq '') {
+    if (!exists $params->{wms_url} || !defined $params->{wms_url} || $params->{wms_url} eq '' || $params->{wms_url} eq 'http://') {
         ERROR("Undefined WMS server URL.");
         return FALSE;
     }
+    $params->{wms_url} =~ s/^http:\/\///;
+    $self->{url} = $params->{wms_url};
+
+
     ## WMS version
     if (!exists $params->{wms_version} || !defined $params->{wms_version} || $params->{wms_version} eq '') {
         ERROR("Undefined WMS protocol version.");
         return FALSE;
+    } elsif (! isWmsVersion($params->{wms_version}; {
+        return FALSE;
     }
+    $self->{version} = $params->{wms_version};
+
+
     ## Layers list
     if (!exists $params->{wms_layers} || !defined $params->{wms_layers} || $params->{wms_layers} eq '') {
         ERROR("Undefined layers list.");
         return FALSE;
     }
+    $self->{layers} = $params->{wms_layers};
+
+
     ## Layer styles list
     if (!exists $params->{wms_styles} || !defined $params->{wms_styles} || $params->{wms_styles} eq '') {
         ERROR("Undefined layer styles list.");
@@ -260,24 +286,126 @@ sub _load() {
         ERROR("The number of layer styles does not match the number of layers.");
         return FALSE;
     }
+    $self->{styles} = $params->{wms_styles};
+
+
     ## Number of color channels
     if (!exists $params->{wms_channels} || !defined $params->{wms_channels} || $params->{wms_channels} eq '') {
         ERROR("Undefined images' number of channels.");
         return FALSE;
+    } elsif (! isPositiveInt($params->{wms_channels})) {
+        ERROR("Channels number must be a positive integer.");
+        return FALSE;
     }
+    $self->{channels} = $params->{wms_channels};
+
+
     ## Background color
     if (!exists $params->{wms_nodata} || !defined $params->{wms_nodata} || $params->{wms_nodata} eq '') {
         ERROR("Undefined nodata value / background color.");
         return FALSE;
     }
+    $self->{nodata} = $params->{wms_nodata};
+
+
     ## Bounding box
     if (!exists $params->{wms_extent} || !defined $params->{wms_extent} || $params->{wms_extent} eq '') {
         ERROR("Undefined data extent.");
         return FALSE;
-    } elsif (scalar (split (',',$params->{wms_styles})) != 4) {
+    } elsif (scalar (split (',',$params->{wms_extent})) != 4) {
         ERROR("Wrong coordinates count in extent. Extent should be 'min_x,min_y,max_x,max_y'");
         return FALSE;
     }
+    foreach my $coord (split (',',$params->{wms_extent})) {
+        if (! isNumber($coord)) {
+            ERROR("Extent coordinates should be decimal numbers.");
+            return FALSE;
+        }
+    }
+    $self->{extent} = $params->{wms_extent};
+
+
+
+    #### Optional parameters ####
+
+    ## wms_proxy - (opt)
+    if (exists $params->{wms_proxy} && defined $params->{wms_proxy} && $params->{wms_proxy} ne '') {
+        $self->{proxy} = $params->{wms_proxy};
+    }
+
+    ## wms_timeout - (opt)
+    if (exists $params->{wms_timeout} && defined $params->{wms_timeout} && $params->{wms_timeout} ne '') {
+        if (! isPositiveInt($params->{wms_timeout})) {
+            ERROR("Request timeout value must be a positive integer.");
+            return FALSE;
+        }
+        $self->{timeout} = $params->{wms_timeout};
+    }
+
+    ## wms_retry - (opt)
+    if (exists $params->{wms_retry} && defined $params->{wms_retry} && $params->{wms_retry} ne '') {
+        if (! isPositiveInt($params->{wms_retry})) {
+            ERROR("Request retry number must be a positive integer.");
+            return FALSE;
+        }
+        $self->{retry} = $params->{wms_retry};
+    }
+
+    ## wms_interval - (opt)
+    if (exists $params->{wms_interval} && defined $params->{wms_interval} && $params->{wms_interval} ne '') {
+        if (! isPositiveInt($params->{wms_interval})) {
+            ERROR("Request retry interval delay must be a positive integer.");
+            return FALSE;
+        }
+        $self->{interval} = $params->{wms_interval};
+    }
+
+    ## wms_user - (opt)
+    if (exists $params->{wms_user} && defined $params->{wms_user} && $params->{wms_user} ne '') {
+        $self->{user} = $params->{wms_user};
+    }
+
+    ## wms_password - (opt)
+    if (exists $params->{wms_password} && defined $params->{wms_password} && $params->{wms_password} ne '') {
+        $self->{password} = $params->{wms_password};
+    }
+
+    ## wms_referer - (opt)
+    if (exists $params->{wms_referer} && defined $params->{wms_referer} && $params->{wms_referer} ne '') {
+        $self->{wms_referer} = $params->{wms_referer};
+    }
+
+    ## wms_userAgent - (opt)
+    if (exists $params->{wms_userAgent} && defined $params->{wms_userAgent} && $params->{wms_userAgent} ne '') {
+        $self->{userAgent} = $params->{wms_userAgent};
+    }
+
+    ## wms_crs - (opt)
+    if (exists $params->{wms_crs} && defined $params->{wms_crs} && $params->{wms_crs} ne '') {
+        $self->{crs} = $params->{wms_crs};
+    }
+
+    ## wms_format - (opt)
+    if (exists $params->{wms_format} && defined $params->{wms_format} && $params->{wms_format} ne '') {
+        if (! isWmsFormat($params->{wms_format})) {
+            return FALSE;
+        }
+        $self->{format} = $params->{wms_format};
+    }  
+
+    ## wms_option - (opt)
+    if (exists $params->{wms_option} && defined $params->{wms_option} && $params->{wms_option} ne '') {
+        $self->{option} = $params->{wms_option};
+    }
+
+
+
+
+
+
+
+
+
 
 
 
@@ -288,7 +416,95 @@ sub _load() {
 #                                        Group: Tests                                              #
 ####################################################################################################
 
+=begin nd
+Function: isWmsFormat
 
+Tests if format value is allowed.
+
+Parameters (list):
+    wmsformat - string - Format value to test
+=cut
+sub isWmsFormat {
+    my $self = shift;
+    my $wmsformat = shift;
+
+    TRACE;
+
+    return FALSE if (! defined $wmsformat);
+
+    foreach (@{$WMS{format}}) {
+        return TRUE if ($wmsformat eq $_);
+    }
+    ERROR (sprintf "Unknown 'wms_format' (%s) !",$wmsformat);
+    return FALSE;
+}
+
+=begin nd
+Function: isWmsVersion
+
+Tests if WMS version value is allowed.
+
+Parameters (list):
+    wmsversion - string - Version value to test
+=cut
+sub isWmsVersion {
+    my $self = shift;
+    my $wmsversion = shift;
+
+    TRACE;
+
+    return FALSE if (! defined $wmsversion);
+
+    foreach (@{$WMS{version}}) {
+        return TRUE if ($wmsversion eq $_);
+    }
+    ERROR (sprintf "Unknown or unsupported 'wms_version' (%s) !",$wmsversion);
+    return FALSE;
+}
+
+=begin nd
+Function: isPositiveInt
+
+Tests if the item is a positive integer (decimal base).
+Source : http://www.perlmonks.org/?node_id=614452
+
+Parameters:
+    item - number/string - the item to test
+
+Returns:
+    The boolean answer to the question.
+=cut
+sub isPositiveInt {
+    my $self = shift;
+    my $item = shift;
+
+    if ($item =~ m/\A\+?[0-9]*[0-9][0-9]*\z/) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+
+=begin nd
+Function: isNumber
+
+Tests if the item is a valid decimal number.
+
+Parameters:
+    item - number/string - the item to test
+
+Returns:
+    The boolean answer to the question.
+=cut
+sub isPositiveInt {
+    my $self = shift;
+    my $item = shift;
+
+    if ($item =~ m/\A[+-]?[0-9]*[0-9][0-9]*[.]?[0-9]*\z/) {
+        return TRUE;
+    }
+    return FALSE;
+}
 
 
 
