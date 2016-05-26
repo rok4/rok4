a83d7aba822bb301f81cc33d6d7f387d09f41105
lgrondin <devnull@localhost>
2016-05-26 09:58:07 +0200

    ajout des fichiers pour le calcul d'exposition

---
+++ lib/libimage/AspectImage.cpp
@@ -0,0 +1,205 @@
+/*
+ * Copyright © (2011) Institut national de l'information
+ *                    géographique et forestière
+ *
+ * Géoportail SAV <geop_services@geoportail.fr>
+ *
+ * This software is a computer program whose purpose is to publish geographic
+ * data using OGC WMS and WMTS protocol.
+ *
+ * This software is governed by the CeCILL-C license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL-C
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ *
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ */
+
+#include "AspectImage.h"
+
+#include "Logger.h"
+
+#include "Utils.h"
+#include <cstring>
+#include <cmath>
+#define DEG_TO_RAD      .0174532925199432958
+#include <string>
+
+int AspectImage::getline ( float* buffer, int line ) {
+    if ( !aspect ) {
+        generate();
+    }
+    return _getline ( buffer, line );
+}
+
+int AspectImage::getline ( uint16_t* buffer, int line ) {
+    if ( !aspect ) {
+        generate();
+    }
+    return _getline ( buffer, line );
+}
+
+int AspectImage::getline ( uint8_t* buffer, int line ) {
+    if ( !aspect ) {
+        generate();
+    }
+    return _getline ( buffer, line );
+}
+
+//definition des variables
+AspectImage::AspectImage (Image* image,  float resolution, std::string algo, float minSlope) :
+    Image ( image->getWidth(), image->getHeight(), 1, image->getBbox() ),
+    origImage ( image ), aspect ( NULL ), resolution (resolution), algo (algo), minSlope (minSlope)
+    {
+
+    matrix[0] = 1 / (8.0*resolution) ;
+    matrix[1] = 2 / (8.0*resolution) ;
+    matrix[2] = 1 / (8.0*resolution) ;
+
+    matrix[3] = 2 / (8.0*resolution) ;
+    matrix[4] = 0 ;
+    matrix[5] = 2 / (8.0*resolution) ;
+
+    matrix[6] = 1 / (8.0*resolution) ;
+    matrix[7] = 2 / (8.0*resolution) ;
+    matrix[8] = 1 / (8.0*resolution) ;
+
+
+}
+
+AspectImage::~AspectImage() {
+    delete origImage;
+    if ( aspect ) delete[] aspect;
+}
+
+
+int AspectImage::_getline ( float* buffer, int line ) {
+    convert ( buffer, aspect + line * width, width );
+    return width;
+}
+
+int AspectImage::_getline ( uint16_t* buffer, int line ) {
+    convert ( buffer, aspect + line * width, width );
+    return width;
+}
+
+int AspectImage::_getline ( uint8_t* buffer, int line ) {
+    memcpy ( buffer, aspect + line * width, width );
+    return width;
+}
+
+
+int AspectImage::getOrigLine ( uint8_t* buffer, int line ) {
+    return origImage->getline ( buffer,line );
+}
+
+int AspectImage::getOrigLine ( uint16_t* buffer, int line ) {
+    return origImage->getline ( buffer,line );
+}
+
+int AspectImage::getOrigLine ( float* buffer, int line ) {
+    return origImage->getline ( buffer,line );
+}
+
+
+void AspectImage::generate() {
+    aspect = new float[origImage->getWidth() * origImage->getHeight()];
+    bufferTmp = new float[origImage->getWidth() * 3];
+    float* lineBuffer[3];
+    lineBuffer[0]= bufferTmp;
+    lineBuffer[1]= lineBuffer[0]+origImage->getWidth();
+    lineBuffer[2]= lineBuffer[1]+origImage->getWidth();
+
+    int line = 0;
+    int nextBuffer = 0;
+    getOrigLine ( lineBuffer[0], line );
+    getOrigLine ( lineBuffer[1], line+1 );
+    getOrigLine ( lineBuffer[2], line+2 );
+    generateLine ( line++, lineBuffer[0],lineBuffer[0],lineBuffer[1] );
+    generateLine ( line++, lineBuffer[0],lineBuffer[1],lineBuffer[2] );
+    while ( line < origImage->getHeight() -1 ) {
+        getOrigLine ( lineBuffer[nextBuffer], line+1 );
+        generateLine ( line++, lineBuffer[ ( nextBuffer+1 ) %3],lineBuffer[ ( nextBuffer+2 ) %3],lineBuffer[nextBuffer] );
+        nextBuffer = ( nextBuffer+1 ) %3;
+    }
+    generateLine ( line,lineBuffer[nextBuffer], lineBuffer[ ( nextBuffer+1 ) %3],lineBuffer[ ( nextBuffer+1 ) %3] );
+    delete[] bufferTmp;
+}
+
+void AspectImage::generateLine ( int line, float* line1, float* line2, float* line3) {
+    float* currentLine =aspect + line * width;
+    //on commence a la premiere colonne
+    int column = 1;
+    //creation de la variable sur laquelle on travaille pour trouver le seuil
+    double value,value1,value2,slope;
+
+    //calcul de la variable sur la premiere colonne
+
+    value1 = (matrix[2] * ( * ( line1+column+1 ) ) + matrix[5] * ( * ( line2+column+1 ) ) + matrix[8] * ( * ( line3+column+1 ) ) - matrix[0] * ( * ( line1+column ) ) - matrix[3] * ( * ( line2+column ) ) - matrix[6] * ( * ( line3+column ) ));
+    value2 = (matrix[0] * ( * ( line1+column ) ) + matrix[1] * ( * ( line1+column ) ) + matrix[2] * ( * ( line1+column+1 ) ) - matrix[6] * ( * ( line3+column ) ) - matrix[7] * ( * ( line3+column ) ) - matrix[8] * ( * ( line3+column+1 ) ));
+
+    //calcul de la pente pour ne pas afficher l'exposition en dessous d'une certaine valeur de pente
+    slope = sqrt(pow(value1,2.0)+pow(value2,2.0));
+    if (slope <= minSlope) {
+        value = -1.0;
+    } else {
+        value = (atan2(value1,value2) + M_PI) * 180 / M_PI;
+    }
+
+    *currentLine = value;
+
+    //calcul de la variable sur toutes les autres colonnes
+    while ( column < width - 1 ) {
+
+        value1 = (matrix[2] * ( * ( line1+column+1 ) ) + matrix[5] * ( * ( line2+column+1 ) ) + matrix[8] * ( * ( line3+column+1 ) ) - matrix[0] * ( * ( line1+column-1 ) ) - matrix[3] * ( * ( line2+column-1 ) ) - matrix[6] * ( * ( line3+column-1 ) ));
+        value2 = (matrix[0] * ( * ( line1+column-1 ) ) + matrix[1] * ( * ( line1+column ) ) + matrix[2] * ( * ( line1+column+1 ) ) - matrix[6] * ( * ( line3+column-1 ) ) - matrix[7] * ( * ( line3+column ) ) - matrix[8] * ( * ( line3+column+1 ) ));
+
+        //calcul de la pente pour ne pas afficher l'exposition en dessous d'une certaine valeur de pente
+        slope = sqrt(pow(value1,2.0)+pow(value2,2.0));
+        if (slope <= minSlope) {
+            value = -1.0;
+        } else {
+            value = (atan2(value1,value2) + M_PI) * 180 / M_PI;
+        }
+
+        * ( currentLine+ ( column++ ) ) = ( value );
+
+    }
+
+
+    //calcul de la variable sur la dernière colonne
+    value1 = (matrix[2] * ( * ( line1+column ) ) + matrix[5] * ( * ( line2+column ) ) + matrix[8] * ( * ( line3+column ) ) - matrix[0] * ( * ( line1+column-1 ) ) - matrix[3] * ( * ( line2+column-1 ) ) - matrix[6] * ( * ( line3+column-1 ) ));
+    value2 = (matrix[0] * ( * ( line1+column-1 ) ) + matrix[1] * ( * ( line1+column ) ) + matrix[2] * ( * ( line1+column ) ) - matrix[6] * ( * ( line3+column-1 ) ) - matrix[7] * ( * ( line3+column ) ) - matrix[8] * ( * ( line3+column ) ));
+
+    //calcul de la pente pour ne pas afficher l'exposition en dessous d'une certaine valeur de pente
+    slope = sqrt(pow(value1,2.0)+pow(value2,2.0));
+    if (slope <= minSlope) {
+        value = -1.0;
+    } else {
+        value = (atan2(value1,value2) + M_PI) * 180 / M_PI;
+    }
+
+    * ( currentLine+column ) = ( value );
+}
+
+
---
+++ lib/libimage/AspectImage.h
@@ -0,0 +1,196 @@
+/*
+ * Copyright © (2011) Institut national de l'information
+ *                    géographique et forestière
+ *
+ * Géoportail SAV <geop_services@geoportail.fr>
+ *
+ * This software is a computer program whose purpose is to publish geographic
+ * data using OGC WMS and WMTS protocol.
+ *
+ * This software is governed by the CeCILL-C license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL-C
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ *
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ */
+
+#ifndef ASPECTIMAGE_H
+#define ASPECTIMAGE_H
+
+#include "Image.h"
+#include <string>
+
+
+class AspectImage : public Image {
+
+private:
+
+    /** \~french
+    * \brief Image d'origine utilisée pour calculer l'exposition
+    ** \~english
+    * \brief Origin image used to compute the aspect
+    */
+    Image* origImage;
+
+    /** \~french
+    * \brief Buffer contenant l'exposition calculée
+    ** \~english
+    * \brief Buffer of the aspect
+    */
+    float* aspect;
+
+    /** \~french
+    * \brief Buffer temporaire
+    ** \~english
+    * \brief Temporary buffer
+    */
+    float* bufferTmp;
+
+    /** \~french
+    * \brief Matrice de convolution
+    ** \~english
+    * \brief Convolution matrix
+    */
+    float matrix[9];
+
+    /** \~french
+    * \brief Résolution de l'image d'origine et donc finale
+    ** \~english
+    * \brief Resolution of the image
+    */
+    float resolution;
+
+    /** \~french
+    * \brief algo : choix de l'algorithme de calcul d'expositions  par l'utilisateur ("H" pour Horn)
+    ** \~english
+    * \brief algo : aspect calculation algorithm chosen by the user ("H" for Horn)
+    */
+    std::string algo;
+
+    /** \~french
+    * \brief minSlope : indique la valeur de pente à partir de laquelle l'exposition est calculee
+    ** \~english
+    * \brief minSlope : indicate the value of slope from which aspect is computed
+    */
+    float minSlope;
+
+
+
+
+    /** \~french
+    * \brief Récupère la ligne
+    ** \~english
+    * \brief Get line
+    */
+    int _getline ( uint8_t* buffer, int line );
+
+    /** \~french
+    * \brief Récupère la ligne
+    ** \~english
+    * \brief Get line
+    */
+    int _getline ( uint16_t* buffer, int line );
+
+    /** \~french
+    * \brief Récupère la ligne
+    ** \~english
+    * \brief Get line
+    */
+    int _getline ( float* buffer, int line );
+
+    /** \~french
+    * \brief Récupère la ligne dans l'image d'origine
+    ** \~english
+    * \brief Get line in origin image
+    */
+    int getOrigLine ( uint8_t* buffer, int line );
+
+    /** \~french
+    * \brief Récupère la ligne dans l'image d'origine
+    ** \~english
+    * \brief Get line in origin image
+    */
+    int getOrigLine ( uint16_t* buffer, int line );
+
+    /** \~french
+    * \brief Récupère la ligne dans l'image d'origine
+    ** \~english
+    * \brief Get line in origin image
+    */
+    int getOrigLine ( float* buffer, int line );
+
+    /** \~french
+    * \brief Génére l'image de l'exposition
+    ** \~english
+    * \brief Generate aspect
+    */
+    void generate();
+
+    /** \~french
+    * \brief Génére une ligne de l'image de l'exposition
+    ** \~english
+    * \brief Generate one line of the aspect
+    */
+    void generateLine ( int line, float* line1, float* line2 , float* line3 );
+
+public:
+
+    /** \~french
+    * \brief Récupère la ligne
+    ** \~english
+    * \brief Get line
+    */
+    virtual int getline ( float* buffer, int line );
+
+    /** \~french
+    * \brief Récupère la ligne
+    ** \~english
+    * \brief Get line
+    */
+    virtual int getline ( uint8_t* buffer, int line );
+
+    /** \~french
+    * \brief Récupère la ligne
+    ** \~english
+    * \brief Get line
+    */
+    virtual int getline ( uint16_t* buffer, int line );
+
+    /** \~french
+    * \brief Constructeur
+    ** \~english
+    * \brief Construtor
+    */
+    AspectImage ( Image* image, float resolution, std::string algo, float minSlope);
+
+    /** \~french
+    * \brief Destructeur
+    ** \~english
+    * \brief Destructor
+    */
+    virtual ~AspectImage();
+
+};
+
+#endif // ASPECTIMAGE_H
