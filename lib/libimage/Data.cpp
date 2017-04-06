67a04236352fda438c614f2f13c8f1e0166aa247
Loic Grondin <loic.grondin@ign.fr>
2017-04-06 09:48:41 +0200

    Remplacement du assert

--- lib/libimage/Data.cpp
+++ lib/libimage/Data.cpp
@@ -49,11 +49,21 @@ BufferedDataSource::BufferedDataSource ( DataStream& dataStream ) : type ( dataS
     // On initialise data à une taille arbitraire de 32Ko.
     size_t maxSize = 32768;
     data = new uint8_t[maxSize];
+    status = true;
 
     while ( !dataStream.eof() ) { // On lit le DataStream jusqu'au bout
-        size_t size = dataStream.read ( data + dataSize, maxSize - dataSize );
+
+        size_t size = dataStream.read ( data + dataSize, maxSize - dataSize);
         dataSize += size;
-        if ( size == 0 || dataSize == maxSize ) { // On alloue 2 fois plus de place si on en manque.
+
+        if (size == 0) {
+            //il y a eu un probleme lors de la lecture
+            status = false;
+            dataSize = 0;
+            break;
+        }
+
+        if ( dataSize == maxSize ) { // On alloue 2 fois plus de place si on en manque.
             maxSize *= 2;
             uint8_t* tmp = new uint8_t[maxSize];
             memcpy ( tmp, data, dataSize );
@@ -62,11 +72,13 @@ BufferedDataSource::BufferedDataSource ( DataStream& dataStream ) : type ( dataS
         }
     }
 
-    // On réalloue exactement la taille nécessaire pour ne pas perdre de place
-    uint8_t* tmp = new uint8_t[dataSize];
+    if (status) {
+        // On réalloue exactement la taille nécessaire pour ne pas perdre de place
+        uint8_t* tmp = new uint8_t[dataSize];
 
-    memcpy ( tmp, data, dataSize );
-    delete[] data;
-    data = tmp;
+        memcpy ( tmp, data, dataSize );
+        delete[] data;
+        data = tmp;
+    }
 }
 
